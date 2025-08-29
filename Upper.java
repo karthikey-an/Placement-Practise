5d6600863724815cc8fe2a008d083727d14b8ef3
karthikey-an <166150230+karthikey-an@users.noreply.github.com>
2025-08-29 14:10:49 +0530

    Add files via upload

---
+++ Upper.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count =0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            if(!Character.isLetterOrDigit(c))
+            {
+                count++;
+            }
+        }
+       System.out.print(count); 
+    }
+}
